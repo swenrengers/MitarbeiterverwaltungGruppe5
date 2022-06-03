326a3bf0ea33372e0d148316606da0a3ca7b9d29
srengers <srengers@uni-bremen.de>
2022-06-03 09:56:07 +0200

    Menüpunkt 2.2 ready

--- Menüstruktur.py
+++ Menüstruktur.py
@@ -1,4 +1,9 @@
 import pandas as pd
+pd.set_option('display.max_columns', 20)
+pd.set_option('display.max_rows', 20)
+pd.options.display.width = 0
+url = 'https://raw.githubusercontent.com/swenrengers/MitarbeiterverwaltungGruppe5/main/export.csv'
+df = pd.read_csv(url, sep=';', index_col=0)
 
 selection = 0       # A variable is added for inputs from the user, to go through while loops.
 while selection != 6:       # As long as the variable is not 6, the while loop will be running.
@@ -42,12 +47,12 @@ while selection != 6:       # As long as the variable is not 6, the while loop w
                 selection = int(input("Please enter a number between 1 and 5!  "))      # The user now can give a new value to the variable.
 
                 if selection == 1:
-                    pd.set_option('display.max_columns', 20)
-                    pd.set_option('display.max_rows', 20)
-                    pd.options.display.width = 0
-                    url = 'https://raw.githubusercontent.com/swenrengers/MitarbeiterverwaltungGruppe5/main/export.csv'
-                    df = pd.read_csv(url, sep=';', index_col=0)
+
                     print(df)
+
+                if selection == 2:
+                    datasetnr = int(input("Which dataset do you want to show?  "))
+                    print(df.iloc[datasetnr])
 
                 if selection != 5:      # If any number except the number 5 is inserted, then the text below will be printed and the submenu will be displayed again.
                     print("\n This function is not developed yet.\n")

727c9d9fa28e7152396b3d673dfc588322043506
karbokle <karbokle@uni-bremen.de>
2022-06-27 23:20:04 +0200

    Neue Dataframes und Coloumns

--- MitarbeiterverwaltungGruppe5.py
+++ MitarbeiterverwaltungGruppe5.py
@@ -1,4 +1,5 @@
 import pandas as pd #Modul für die Datenbankverwaltung
+import datetime
 
 pd.set_option('display.max_columns', 20) #zur Übersichtlichkeit
 pd.set_option('display.max_rows', 20)
@@ -23,18 +24,75 @@ while selection != 6:       # As long as the variable is not 6, the while loop w
                               "Input: "))
 
         if(selection == 1):     # If the value of the variable is a 1, then this while loop will start.
-            while selection != 4:       # The while loop is running, as long as not the number 4 is inserted.
-                print("\n""1. add datasets from file (merge)\n"     # The submenu is printed.
-                      "2. add new single dataset (via console)\n"
-                      "3. add new column (for all datasets)\n"
-                      "4. back\n")
+            while selection != 3:       # The while loop is running, as long as not the number 4 is inserted.
+                print("\n""1. add new single dataset (via console)\n"     # The submenu is printed.
+                      "2. add new column (for all datasets)\n"
+                      "3. back\n")
 
-                selection = int(input("Please enter a number between 1 and 4!  "))      # The user now can give a new value to the variable.
+                selection = int(input("Please enter a number between 1 and 3!  "))      # The user now can give a new value to the variable.
 
-                if selection != 4:      # If any number except the number 4 is entered, then the text below will be printed and the submenu will be displayed again.
-                    print("\n This function is not developed yet.\n")
+                if selection == 1:
 
-                if selection > 4:       # If any number above 4 is entered, then the text below is printed out.
+                    nr = int(input("ID: ")),
+                    name = input("Name: "),
+                    personalnummer = int(input("Personalnummern: ")),
+                    day = int(input("Geburtstag: "))
+                    month = int(input("Geburtsmonat "))
+                    year = int(input("Geburtsjahr: "))
+                    geburtsdatum = datetime.date(day, month, year)
+                    alter = int(input("Alter: ")),
+                    adresse = input("Adresse: "),
+                    abteilung = input("Abteilung: "),
+                    telefonnummer = int(input("Telefonnummer ")),
+                    anstellungsverhaeltnis = input("Anstellungsverhältnis: "),
+                    geschlecht = input("Geschlecht (m/w/d): "),
+                    fuererschein = input("Führerschein: "),
+                    religion = input("Religion: "),
+                    krankenkasse = input("Krankenkasse: "),
+                    familienstand = input("Familienstand: "),
+                    gehalt = int(input("Gehalt: ")),
+                    email = input("E-Mail: "),
+                    vorgesetzter = input("Vorgesetzter: "),
+                    day = int(input("Eintrittstag: "))
+                    month = int(input("Eintrittsmonat "))
+                    year = int(input("Eintrittsjahr: "))
+                    eintrittsdatum = datetime.date(day, month, year),
+
+                    newdataframe = {
+                        'ID': [nr],
+                        'Name': [name],
+                        'Personalnummer': [personalnummer],
+                        'Geburtsdatum': [geburtsdatum],
+                        'Alter': [alter],
+                        'Adresse': [adresse],
+                        'Abteilung': [abteilung],
+                        'Telefonnummer': [telefonnummer],
+                        'Anstellungsverhältnis': [anstellungsverhaeltnis],
+                        'Geschlecht (m/w/d)': [geschlecht],
+                        'Führerschein': [fuererschein],
+                        'Religion': [religion],
+                        'Krankenkasse': [krankenkasse],
+                        'Familienstand': [familienstand],
+                        'Gehalt': [gehalt],
+                        'E-Mail': [email],
+                        'Vorgesetzter': [vorgesetzter],
+                        'Eintrittsdatum': [eintrittsdatum]
+                    }
+
+                    df = pd.DataFrame(newdataframe)
+
+                    df.to_csv(url, mode='a', index=False, header=False)
+
+                    print("Data appended successfully.")
+
+                if selection == 2:
+                    newcoloumn = input("Neue Rubrik: ")
+                    df.insert(0, newcoloumn, '')
+                    df.to_csv(url, mode='a', index=False, header=False)
+
+                    print("Coloumn appended successfully.")
+
+                if selection > 3:       # If any number above 4 is entered, then the text below is printed out.
                     print("\nThis number is not valid. Please try again.\n")
 
         elif(selection == 2):       # If the value of the variable is a 2, then this while loop will start.
